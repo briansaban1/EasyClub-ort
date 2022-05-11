49ab0682a20ad83b61e0e3e8751dc5a73a24772b
Brian <info.tododeusa@gmail.com>
2022-05-11 17:40:18 -0300

    update mercadopago

--- app/screens/pago/index.js
+++ app/screens/pago/index.js
@@ -10,9 +10,11 @@ import DateTimePicker from 'react-native-modal-datetime-picker';
 import moment, { min } from 'moment';
 import 'moment/locale/es'
 import WService from '../../service/WebService';
-import MercadoPagoWebTokenizeCheckout from '@blackbox-vision/react-native-mercadopago-tokenize-checkout';
 import { useNavigation } from '@react-navigation/native';
 import Screens from '../../constants/screens';
+import MercadoPagoCheckout from '@blackbox-vision/react-native-mercadopago-px';
+import * as MercadoPagoService from './mercadopago-service';
+import { safeGetOr } from '../../utils/fp';
 
 
 function PagoReserva(data) {
@@ -26,26 +28,98 @@ function PagoReserva(data) {
     console.log( data.route.params.fec , 'fecha')
     console.log( data.route.params.hr , 'hora')
 
-    const [idAct, setIdAct] = useState(data.route.params.id)
+    const [idActividad, setIdActividad] = useState(data.route.params.id)
     const [nombreAct, setNombreAct] = useState(data.route.params.nombre)
     const [precio, setPrecio] = useState(data.route.params.valor)
     const [fechas, setFechas] = useState(data.route.params.fec)
-    const [hor, setHor] = useState(data.route.params.hr)
+    const [hora, setHora] = useState(data.route.params.hr)
+    const fechaArregada = moment(fechas).format('DD/MM/YYYY')
 
 
     const { navigate } = useNavigation();
 
+console.log( nombreAct)
 
+const profile = useSelector(store => store.user.profile)
 
-    const profile = useSelector(store => store.user.profile)
+const id = safeGetOr('', 'id_usuario')(profile)
 
-    const _actividades = useSelector(store => store.user.actividades)
-    const [actividades, setActividades] = useState(_actividades)
-
-    
 
     const wservice = new WService();
 
+    useEffect(() => {
+        startCheckout(id, idActividad, hora, fechaArregada)
+
+    }),[];
+
+    const [paymentResult, setPaymentResult] = useState(null);
+
+const startCheckout = async ({id, idActividad, hora, fechaArregada}) => {
+      try {
+        const preferenceId = await MercadoPagoService.getPreferenceId('payer@email.com', {
+          title: nombreAct,
+          description: nombreAct,
+          quantity: 1,
+          currency_id: 'ARS',
+          unit_price: 100,
+        });
+  
+        const payment = await MercadoPagoCheckout.createPayment({
+          publicKey: 'TEST-edd07fe8-f948-4274-8900-c08bc5fc01a5',
+          preferenceId,
+        });
+
+        console.log(payment)
+
+        if (payment.status === 'approved'){
+            console.log(payment)
+            setPaymentResult(payment)
+            generarReserva(id, idActividad, hora, fechaArregada);
+            onclose();
+            navigate(Screens.ExitoReserva)
+        };
+        if(payment.status === 'rejected'){
+
+            Alert.alert(
+                'Pago Rechazado',
+                'Por favor reintentá el pago. Si el error persiste, por favor contactanos'
+              );
+              navigate(Screens.Reservas)
+
+        };
+         
+
+      } catch (err) {
+        Alert.alert('Algo salió mal', err.message);
+      }
+    };
+
+
+        //se carga la reserva a la base de datos
+        function generarReserva(id, idActividad, hora) {
+
+            wservice.cargarReserva({
+                id,
+                idActividad,
+                hora,
+                //se pasa la fecha en otro formato para que lo tome la api
+                fechaphp: moment(fechas).format('YYYY-MM-DD'),
+            }).then(response => {
+                if (response.status == 1) {
+                    //si la carga es exitosa se redirecciona a la ventana de exito
+                    //goExito()
+                    console.log(response.status, 'ESTADO PAGO')
+                   
+                    //se redirecciona a la pantalla de exito
+                    navigate(Screens.ExitoReserva)
+                    //se borran todas las propiedades
+                    setIdActividad('')
+                    setHora('')
+                    setFechas('')
+
+                }
+            })
+        }
 
 
 
@@ -60,7 +134,7 @@ function PagoReserva(data) {
 
                 <View style={{ height: 20 }} />
 
-                
+      <Text style={styles.text}>{paymentResult}</Text>
 
                 <View>
 
