715ae81fb67676c8ac03b7ee46b1f7c7bd021c61
Brian <info.tododeusa@gmail.com>
2022-05-20 15:15:03 -0300

    update styles

--- app/screens/create-activity/styles.js
+++ app/screens/create-activity/styles.js
@@ -98,6 +98,64 @@ text2: {
   paddingHorizontal: 30,
 
 },
+text3: {
+  color: Colors.blue400,
+  fontSize: Dimensions.px15,
+  paddingVertical: 10,
+  marginBottom:0,
+  paddingHorizontal: 0,
+
+},
+buttonCalendar: {
+
+  width: '85%',
+  height: 50,
+  borderBottomWidth: 3,
+  borderColor: '#154DDE',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  flexDirection: 'row',
+  paddingHorizontal: 15,
+  borderRadius: 5,
+  backgroundColor: '#F0f6fd',
+  marginBottom: 15,
+},
+textDate: {
+  textTransform: 'uppercase',
+  color: Colors.blue400,
+  fontWeight: 'bold',
+  fontSize: 14
+},
+timeContainer: {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  width: '100%',
+  height: 50,
+  borderColor: '#DBE3Eb',
+  borderBottomWidth: 1,
+  paddingBottom: 1,
+  //backgroundColor:'#DBE3Eb',
+  paddingHorizontal: 20,
+  //alignSelf:'center'
+},
+textTime: {
+  color: Colors.blue400,
+  fontSize: Dimensions.px14,
+},
+selected: {
+  borderColor: '#A8B3C8',
+  borderWidth: 1,
+  //width: '50%',
+  borderRadius: 5,
+  height: 60,
+  //borderStyle: 'dotted',
+  //backgroundColor: '#f',
+  justifyContent: 'center',
+  backgroundColor: '#EBEFF3',
+  //shadowOffset:'5',
+  //shadowOpacity: '#f'
+},
 });
 
 export default styles;
